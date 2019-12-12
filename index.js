991ed587d9a05385f21f360df93d78c9717ffa27
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 20:44:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ class Formatter {
     static sanitize(string){
   return string.replace(/[^A-Za-z0-9-']+/g, '');
   }
+   static titleize(string){
+  return string.replace(/[^A-Za-z0-9-']+/g, '');
+  }
 }
