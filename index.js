98962786ce67dca0dd9eb547f3356bdea51ebdd9
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 21:21:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ class Formatter {
   static capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-    static sanitize(string){
-  return string.replace(/[^A-Za-z0-9-']+/g, '');
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
   }
    static titleize(string){
     let g= string.split(" ");
