2ac9d42a987acfc52e97bfe2783f85b44a8d5cce
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 21:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ class Formatter {
       }else if(g[i]=='the' ||g[i]=='a'||g[i]=='an'||g[i]=='but'||g[i]=='of'||g[i]=='and'||g[i]=='for'||g[i]=='at'||g[i]=='by'||g[i]=='form' ){
         all.push(g[i])
       }else{
-        let cap=g[i].toUpperCase()
-        all.push(cap)
+        let cap=g[i]
+        all.push(cap.toUpperCase())
       }
     }
     let u= all.join()
