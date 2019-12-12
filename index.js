acd8e30ea487a118c3f02b925760281818ea8107
Sarah Alafif <sar1997yah@gmail.com>
2019-12-12 20:43:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Formatter {
   static capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-    static capitalize(string){
+    static sanitize(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 }
